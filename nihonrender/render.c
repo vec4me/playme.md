2eebc2e01060486dca60eeaad10b99bcb706b42a
Jeff Skinner <jeff@je.gy>
2022-10-15 02:50:24 +0900

    No more floats! Integer rendering somewhat done

--- nihonrender/render.c
+++ nihonrender/render.c
@@ -1,167 +1,106 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define w 320
 #define h 200
 
-static int pixels[w*h];
-
-static int CUR_COLOR = 0;
-
-#define SET_COLOR(R, G, B) CUR_COLOR = R | G<<8 | B<<16
-#define COLOR_PIXEL(X, Y, w) pixels[X + Y*w] = CUR_COLOR
-
-#define MIX(A, B, X) A + X*(B - A)
-#define DOT(AX, AY, AZ, BX, BY, BZ) AX*BX + AY*BY + AZ*BZ
-
-static int PL_sin[256] = {
-	0x0000, 0x0324, 0x0647, 0x096a, 0x0c8b, 0x0fab, 0x12c8, 0x15e2,
-	0x18f8, 0x1c0b, 0x1f19, 0x2223, 0x2528, 0x2826, 0x2b1f, 0x2e11,
-	0x30fb, 0x33de, 0x36ba, 0x398c, 0x3c56, 0x3f17, 0x41ce, 0x447a,
-	0x471c, 0x49b4, 0x4c3f, 0x4ebf, 0x5133, 0x539b, 0x55f5, 0x5842,
-	0x5a82, 0x5cb4, 0x5ed7, 0x60ec, 0x62f2, 0x64e8, 0x66cf, 0x68a6,
-	0x6a6d, 0x6c24, 0x6dca, 0x6f5f, 0x70e2, 0x7255, 0x73b5, 0x7504,
-	0x7641, 0x776c, 0x7884, 0x798a, 0x7a7d, 0x7b5d, 0x7c29, 0x7ce3,
-	0x7d8a, 0x7e1d, 0x7e9d, 0x7f09, 0x7f62, 0x7fa7, 0x7fd8, 0x7ff6,
-	0x8000, 0x7ff6, 0x7fd8, 0x7fa7, 0x7f62, 0x7f09, 0x7e9d, 0x7e1d,
-	0x7d8a, 0x7ce3, 0x7c29, 0x7b5d, 0x7a7d, 0x798a, 0x7884, 0x776c,
-	0x7641, 0x7504, 0x73b5, 0x7255, 0x70e2, 0x6f5f, 0x6dca, 0x6c24,
-	0x6a6d, 0x68a6, 0x66cf, 0x64e8, 0x62f2, 0x60ec, 0x5ed7, 0x5cb4,
-	0x5a82, 0x5842, 0x55f5, 0x539b, 0x5133, 0x4ebf, 0x4c3f, 0x49b4,
-	0x471c, 0x447a, 0x41ce, 0x3f17, 0x3c56, 0x398c, 0x36ba, 0x33de,
-	0x30fb, 0x2e11, 0x2b1f, 0x2826, 0x2528, 0x2223, 0x1f19, 0x1c0b,
-	0x18f8, 0x15e2, 0x12c8, 0x0fab, 0x0c8b, 0x096a, 0x0647, 0x0324
-};
-
-static float isqrt(float n) {
-	union {
-		int i;
-		float f;
-	} u = {.f = n};
-
-	u.i = 0x5f3759df - (u.i >> 1);
-	u.f *= 0.5f*(3 - n*u.f*u.f);
-
-	return u.f;
-}
+#define dot(x, y, z, i, j, k) x*i + y*j + z*k
 
 int main(int argc, char **argv) {
-	float cx = 0;
-	float cy = 1.72f;
-	float cz = 0;
+	static int sin[] = {0, 3, 6, 9, 12, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 51, 54, 57, 60, 63, 65, 68, 71, 73, 76, 78, 81, 
+ 83, 85, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 107, 109, 111, 112, 113, 115, 116, 117, 118, 120, 121, 122, 
+ 122, 123, 124, 125, 125, 126, 126, 126, 127, 127, 127, 127, 127, 127, 127, 126, 126, 126, 125, 125, 124, 123, 
+ 122, 122, 121, 120, 118, 117, 116, 115, 113, 112, 111, 109, 107, 106, 104, 102, 100, 98, 96, 94, 92, 90, 88, 
+ 85, 83, 81, 78, 76, 73, 71, 68, 65, 63, 60, 57, 54, 51, 49, 46, 43, 40, 37, 34, 31, 28, 25, 22, 19, 16, 12, 9, 
+ 6, 3, 0, -3, -6, -9, -12, -16, -19, -22, -25, -28, -31, -34, -37, -40, -43, -46, -49, -51, -54, -57, -60, -63, 
+ -65, -68, -71, -73, -76, -78, -81, -83, -85, -88, -90, -92, -94, -96, -98, -100, -102, -104, -106, -107, -109, 
+ -111, -112, -113, -115, -116, -117, -118, -120, -121, -122, -122, -123, -124, -125, -125, -126, -126, -126, 
+ -127, -127, -127, -127, -127, -127, -127, -126, -126, -126, -125, -125, -124, -123, -122, -122, -121, -120, 
+ -118, -117, -116, -115, -113, -112, -111, -109, -107, -106, -104, -102, -100, -98, -96, -94, -92, -90, -88, 
+ -85, -83, -81, -78, -76, -73, -71, -68, -65, -63, -60, -57, -54, -51, -49, -46, -43, -40, -37, -34, -31, -28, 
+ -25, -22, -19, -16, -12, -9, -6, -3};
 
-	float ry = 12314;
+	int cos[sizeof sin];
+	for (int i = sizeof sin; i--;) cos[i] = sin[(i + 65)&255];
 
-	float sdx = 1;
-	float sdy = 0;
-	float sdz = 0;
+	enum { cx, cy, cz, ry, lx, ly, lz };
 
-	if (argc > 1) {
-		cx = (float)atof(argv[1]);
-		cz = (float)atof(argv[2]);
+	int arg[7];
+	for (int i = argc; --i;) arg[i - 1] = (int)atoi(argv[i]);
 
-		ry = (float)atof(argv[3]);
-
-		sdx = (float)atof(argv[4]);
-		sdy = (float)atof(argv[5]);
-		sdz = (float)atof(argv[6]);
-	}
-
-	float c_ry = cosf(ry);
-	float s_ry = sinf(ry);
-
-	float fk = 0.000000001f;
-
-	float fl = (1 - powf(fk, cy))/logf(fk);
-	float fh = (powf(fk, 10000) - 1)/logf(fk);
-
-	float AR = 0.941176f;
-	float AG = 0.929412f;
-	float AB = 0.854902f;
+	int cry = cos[arg[ry]&255];
+	int sry = sin[arg[ry]&255];
 
 	printf("P6\n%i %i\n255\n", w, h);
 
 	for (int i = w*h; i--;) {
 		int ix = i%w;
-		int iy = i/w;
+		int iy = (int)(i/w);
 
-		float tx = (w - 2*(float)ix)/h;
-		float ty = 2*(float)iy/h - 1;
+		int ox = w - (ix<<1);
+		int oy = (iy<<1) - h;
 
-		float dx = tx*c_ry - s_ry;
-		float dy = ty;
-		float dz = tx*s_ry + c_ry;
+		int dx = ox*cry/h - sry;
+		int dy = (oy<<8)/h;
+		int dz = ox*sry/h + cry;
 
-		float is = isqrt(dx*dx + dy*dy + dz*dz);
+		int r = 0;
+		int g = 0;
+		int b = 0;
 
-		dx *= is;
-		dy *= is;
-		dz *= is;
+		if (dy > 0) {
+			int d = 70000/dy;
 
-		float f;
+			int hx = arg[cx] + d*dx;
+			int hz = arg[cz] + d*dz;
 
-		float R;
-		float G;
-		float B;
+			r = 188;
+			g = 0;
+			b = 45;
 
-		if (dy < 0) {
-			float z = cy/-dy;
-
-			float hx = cx + z*dx;
-			float hz = cz + z*dz;
-
-			f = fminf(fl/dy, 1);
-
-			R = 0.3f;
-			G = (1 + sinf(4*hx))/6 + 0.2f;
-			B = 0;
-
-			if ((int)hx%16 < 2 || (int)hz%24 < 1.5) {
-				R = 0.2f;
-				G = 0.2f;
-				B = 0.2f;
+			int sky = cos[(cos[(hz>>11)&255] + (hx>>8))>>1&255] + cos[hz/500&255]/4 + 30;
+			if (sky < 0) {
+				r = sky;
+				g = sky;
+				b = sky;
+			}
+			else if (dot(dx, dy, dz, lx, ly, lz) < 3053) {
+				r = 128 - 128*dy/255;
+				g = 179 - 179*dy/255;
+				b = 255 - 76*dy/255;
 			}
 		}
-		else {
-			float z = 1000/dy;
+		else if (dy < 0) {
+			int d = arg[cy]/-dy;
 
-			float hx = cx + z*dx;
-			float hz = cz + z*dz;
+			int hx = arg[cx] + d*dx;
+			int hz = arg[cz] + d*dz;
 
-			float l = DOT(dx, dy, dz, sdx, sdy, sdz);
+			r = 77;
+			g = 40;
+			b = 0;
 
-			f = fminf(fh/dy, 1);
-
-			R = 0.737f;
-			G = 0;
-			B = 0.176f;
-
-			if (l < 0.98f) {
-				R = 0.5f - 0.5f*dy;
-				G = 0.7f - 0.7f*dy;
-				B = 1.0f - 0.3f*dy;
+			if (!(((hx>>12)%6)*((hz>>13)%9))) {
+				r = 100;
+				g = 100;
+				b = 110;
 			}
+			else {
+				r = 60;
+				g = sin[hx/20&255]/2 + 55;
+				b = 0;
 
-			float clouds = sinf(0.005f*hx + 10*sinf(0.0005f*hz));
-			if (clouds > 0) {
-				R = 1 - 0.2f*clouds;
-				G = 1 - 0.2f*clouds;
-				B = 1 - 0.2f*clouds;
+				if (g < 0) {
+					g = 60;
+					b = 120;
+				}
 			}
 		}
 
-		SET_COLOR(
-			(int)(255*(MIX(R, AR, f))),
-			(int)(255*(MIX(G, AG, f))),
-			(int)(255*(MIX(B, AB, f)))
-		);
-
-		COLOR_PIXEL(ix, iy, w);
-
-		putchar(pixels[i]&255);
-		putchar(pixels[i]>>8&255);
-		putchar(pixels[i]>>16&255);
+		putchar(r);
+		putchar(g);
+		putchar(b);
 	}
 
 	return 0;
--- serve.c
+++ serve.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#include <math.h>
+
+#define endof(a) sizeof a - 1
+
+static int sin[] = {0, 3, 6, 9, 12, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 51, 54, 57, 60, 63, 65, 68, 71, 73, 76, 78, 81, 
+ 83, 85, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 107, 109, 111, 112, 113, 115, 116, 117, 118, 120, 121, 122, 
+ 122, 123, 124, 125, 125, 126, 126, 126, 127, 127, 127, 127, 127, 127, 127, 126, 126, 126, 125, 125, 124, 123, 
+ 122, 122, 121, 120, 118, 117, 116, 115, 113, 112, 111, 109, 107, 106, 104, 102, 100, 98, 96, 94, 92, 90, 88, 
+ 85, 83, 81, 78, 76, 73, 71, 68, 65, 63, 60, 57, 54, 51, 49, 46, 43, 40, 37, 34, 31, 28, 25, 22, 19, 16, 12, 9, 
+ 6, 3, 0, -3, -6, -9, -12, -16, -19, -22, -25, -28, -31, -34, -37, -40, -43, -46, -49, -51, -54, -57, -60, -63, 
+ -65, -68, -71, -73, -76, -78, -81, -83, -85, -88, -90, -92, -94, -96, -98, -100, -102, -104, -106, -107, -109, 
+ -111, -112, -113, -115, -116, -117, -118, -120, -121, -122, -122, -123, -124, -125, -125, -126, -126, -126, 
+ -127, -127, -127, -127, -127, -127, -127, -126, -126, -126, -125, -125, -124, -123, -122, -122, -121, -120, 
+ -118, -117, -116, -115, -113, -112, -111, -109, -107, -106, -104, -102, -100, -98, -96, -94, -92, -90, -88, 
+ -85, -83, -81, -78, -76, -73, -71, -68, -65, -63, -60, -57, -54, -51, -49, -46, -43, -40, -37, -34, -31, -28, 
+ -25, -22, -19, -16, -12, -9, -6, -3};
 
 int main() {
+	int cos[sizeof sin];
+	for (int i = sizeof sin; i--;) cos[i] = sin[(i + 65)&255];
+
 	struct sockaddr_in info_u;
 	info_u.sin_family = AF_INET;
 	info_u.sin_port = htons(7890);
 	info_u.sin_addr.s_addr = INADDR_ANY;
 
-	int32_t sock_u = socket(AF_INET, SOCK_STREAM, 0);
+	int sock_u = socket(AF_INET, SOCK_STREAM, 0);
 	bind(sock_u, (struct sockaddr *)&info_u, sizeof info_u);
 	listen(sock_u, 0);
 
 	struct sockaddr_in info_t;
-	uint32_t info_t_s = sizeof info_t;
+	unsigned info_t_s = sizeof info_t;
 
-	float cx = 2000;
-	float cz = 2000;
-
-	float ry = 0;
-
-	float sdx = 1;
-	float sdy = 0;
-	float sdz = 0;
+	enum { cx, cy, cz, ry, sdx, sdy, sdz, n_acts };
+	int arg[] = { 0, 10000, 0, 30, 0, 0, 255 };
 
 	char buff_i[6]; // The request should only be 3 letters long (GET) or this will break completely
-	char buff_o[30000] = "HTTP/1.1 200 OK\r\nCache-Control: max-age=0\r\nConnection: close\r\nContent-Type: image/gif\r\n\r\n";
-	size_t head_l = 89;
+	char buff_o[100000] = "HTTP/1.1 200 OK\r\nCache-Control: max-age=0\r\nConnection: close\r\nContent-Type: image/gif\r\n\r\n";
+	size_t head_s = 89;
+
+	char cmd[255];
 
 	for (;;) {
-		int32_t sock_t = accept(sock_u, (struct sockaddr *)&info_t, &info_t_s);
+		int sock_t = accept(sock_u, (struct sockaddr *)&info_t, &info_t_s);
 		recv(sock_t, buff_i, sizeof buff_i, 0);
 		char type = buff_i[5];
 
@@ -38,30 +51,30 @@ int main() {
 		putchar('\n');
 
 		if (type == 'V') {
-			FILE *hand = popen("./nihonrender/render | ffmpeg -loglevel 0 -i - -f gif -sws_dither bayer -", "r");
-			memset(buff_o + head_l, 0, sizeof buff_o - head_l);
-			fread(buff_o + head_l, sizeof buff_o - head_l, 1, hand);
+			sprintf(cmd, "./nihonrender/render %f %f %f %f %f %f %f | ffmpeg -loglevel 0 -i - -f gif -sws_dither bayer -", (double)arg[cx], (double)arg[cy], (double)arg[cz], (double)arg[ry], (double)arg[sdx], (double)arg[sdy], (double)arg[sdz]);
+
+			FILE *hand = popen(cmd, "r");
+			fread(buff_o + head_s, sizeof buff_o - head_s, 1, hand);
 			pclose(hand);
 
-			char *term = buff_o + sizeof buff_o - 1;
-			while (*term-- != ';') {}
+			char *term = buff_o + endof(buff_o);
+			while (*--term != ';');
 
-			send(sock_t, buff_o, (size_t)(term - buff_o + 2), 0);
+			send(sock_t, buff_o, 1 + (size_t)(term - buff_o), 0);
 		}
 		else {
 			if (type == 'R') {
-				ry += 0.78539816339f;
+				arg[ry] += 32;
 			}
 			if (type == 'L') {
-				ry -= 0.78539816339f;
+				arg[ry] -= 32;
 			}
 			if (type == 'U') {
-				cx -= 2*sinf(ry);
-				cz += 2*cosf(ry);
+				arg[cx] -= 33*sin[arg[ry]&255];
+				arg[cz] += 33*cos[arg[ry]&255];
 			}
 			if (type == 'D') {
-				cx += sinf(ry);
-				cz -= cosf(ry);
+				arg[ry] += 64;
 			}
 
 			send(sock_t, "HTTP/1.1 302 Found\r\nConnection: close\r\nLocation: https://github.com/blocksrey\r\n\r\nWHAT THE FUCK", 94, 0);
